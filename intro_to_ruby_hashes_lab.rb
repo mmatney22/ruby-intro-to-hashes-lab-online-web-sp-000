d04c13534adb2262587a846bddcdca45bd50a045
Morgan Matney <morgan.matney89@gmail.com>
2020-08-03 00:13:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,9 +12,9 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :name => {},
+    railroads: {
+      pieces: 4,
+      names: {},
       :rent_in_dollars => {}
     }
   }
